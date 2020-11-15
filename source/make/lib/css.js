49d651747ec111038e448976906c8e72ba8fc950
octoxalis <octoxalis@gmail.com>
2020-11-15 15:18:25 +0100

    pre-minify css

--- source/make/lib/css.js
+++ source/make/lib/css.js
@@ -8,7 +8,16 @@ module.exports =
     ...args_
   )
   {
-    return css_s.replace( /,\n__\s+/g, '\n' )
+    return (
+      css_s
+      .replace( /\/\*(=| )?[\s\S]*?(=| )?\*\//g, '' )    //: comments
+      .replace( /,\n__\s+/g, '' )    //: 1st
+      //????.replace( /__\s\s[\s\S]+\}$/g, '' )    //: 2nd
+      .replace( /,\n/g, ',' )
+      .replace( /\n{2,16}/g, '\n' )
+      .replace( / {2,16}/g, ' ' )    //: space
+      .replace( /\n\s+([+\-\*\/])\s+/g, ' $1 ' )    //: multiline calc()
+    )
   }
 ,
 
