cfe65cde0a14c20a37ab22b86f7afbd5b3254d73
octoxalis <octoxalis@gmail.com>
2020-11-15 16:42:34 +0100

    pre-minify css without Terser

--- source/make/lib/css.js
+++ source/make/lib/css.js
@@ -11,8 +11,8 @@ module.exports =
     return (
       css_s
       .replace( /\/\*(=| )?[\s\S]*?(=| )?\*\//g, '' )    //: comments
-      .replace( /,\n__\s+/g, '' )    //: 1st
-      //????.replace( /__\s\s[\s\S]+\}$/g, '' )    //: 2nd
+      .replace( /,\n__\s+/g, '' )         //: 1st, default rule
+      //.replace( /__\s+[^\n]+\n/g, '' )    //: 2nd, orphan rule
       .replace( /,\n/g, ',' )
       .replace( /\n{2,16}/g, '\n' )
       .replace( / {2,16}/g, ' ' )    //: space
